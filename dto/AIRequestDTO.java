8a749cef82171d0f56511e5bc1ca1351b7015225
LAPTOP-94TVEIEF\ASUS <3072125674@qq.com>
2026-04-09 18:09:17 +0800

    backend init

---
+++ dto/AIRequestDTO.java
@@ -0,0 +1,12 @@
+package com.lawstudy.dto;
+
+import lombok.Data;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Data
+public class AIRequestDTO implements Serializable {
+    
+    @NotBlank(message = "问题不能为空")
+    private String question;
+}
