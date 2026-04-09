5f865e05bfc628c887c28da044d5b0fdc144ae23
LAPTOP-94TVEIEF\ASUS <3072125674@qq.com>
2026-04-09 18:11:25 +0800

    frontend init

--- backend/service/CourseService.java
+++
@@ -1,7 +0,0 @@
-package com.lawstudy.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.lawstudy.entity.Course;
-
-public interface CourseService extends IService<Course> {
-}
